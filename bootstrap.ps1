e95cd611b0bf545bf9600ce5c21e72f9ba2b0ee3
tommyquant <51040159+tommyquant@users.noreply.github.com>
2020-04-18 23:03:51 +1000

    Create bootstrap.ps1

---
+++ bootstrap.ps1
@@ -0,0 +1,230 @@
+###############################################################################
+# General Windows settings
+###############################################################################
+
+Write-Host "Configuring general settings..." -ForegroundColor "Yellow"
+
+# Explorer: Show hidden files by default: Show Files: 1, Hide Files: 2
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "Hidden" 1
+
+# Explorer: Show file extensions by default
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "HideFileExt" 0
+
+# Taskbar: Hide Cortana
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "ShowCortanaButton" 0
+
+# Taskbar: Hide search
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" "SearchboxTaskbarMode" 0
+
+# Taskbar: Hide Task View
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "ShowTaskViewButton" 0
+
+# Disable User Account Control
+New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
+
+###############################################################################
+# Install packages
+###############################################################################
+
+# Install Chocolatey
+Write-Host "Installing Chocolatey..." -ForegroundColor "Yellow"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Refresh PATH so we can use `choco` command
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
+# Install packages
+Write-Host "Installing packages..." -ForegroundColor "Yellow"
+choco install chocolateygui -y
+choco install firefox -y
+choco install googlechrome -y
+
+# Install development packages
+choco install notepadplusplus.install -y
+choco install vscode -y
+choco install docker-desktop --pre -y # Use edge version so we can use Docker within WSL2
+
+# Install miscellaneous packages
+choco install adobereader -y
+choco install 7zip.install -y
+choco install bitwarden -y
+choco install k-litecodecpackfull -y
+choco install audacity -y
+
+###############################################################################
+# Enable WSL
+###############################################################################
+
+Write-Host "Enabling WSL and Virtual Machine Platform..." -ForegroundColor "Yellow"
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+###############################################################################
+# Uninstall pre-installed apps
+###############################################################################
+
+Write-Host "Uninstalling pre-installed apps..." -ForegroundColor "Yellow"
+
+# Uninstall 3D Builder
+Get-AppxPackage "Microsoft.3DBuilder" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.3DBuilder" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall 3D Viewer
+Get-AppxPackage "Microsoft.Microsoft3DViewer" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.Microsoft3DViewer" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Alarms and Clock
+Get-AppxPackage "Microsoft.WindowsAlarms" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.WindowsAlarms" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Autodesk Sketchbook
+Get-AppxPackage "*.AutodeskSketchBook" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "*.AutodeskSketchBook" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Bing Finance
+Get-AppxPackage "Microsoft.BingFinance" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.BingFinance" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Bing News
+Get-AppxPackage "Microsoft.BingNews" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.BingNews" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Bing Sports
+Get-AppxPackage "Microsoft.BingSports" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.BingSports" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Bing Weather
+Get-AppxPackage "Microsoft.BingWeather" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.BingWeather" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Bubble Witch 3 Saga
+Get-AppxPackage "king.com.BubbleWitch3Saga" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "king.com.BubbleWitch3Saga" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Calendar and Mail
+Get-AppxPackage "Microsoft.WindowsCommunicationsApps" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.WindowsCommunicationsApps" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Candy Crush Friends
+Get-AppxPackage "king.com.CandyCrushFriends" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "king.com.CandyCrushFriends" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Candy Crush Soda Saga
+Get-AppxPackage "king.com.CandyCrushSodaSaga" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "king.com.CandyCrushSodaSaga" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Disney Magic Kingdoms
+Get-AppxPackage "*.DisneyMagicKingdoms" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "*.DisneyMagicKingdoms" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Dolby
+Get-AppxPackage "DolbyLaboratories.DolbyAccess" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "DolbyLaboratories.DolbyAccess" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Facebook
+Get-AppxPackage "*.Facebook" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "*.Facebook" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Farm Heroes Saga
+Get-AppxPackage "king.com.FarmHeroesSaga" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "king.com.FarmHeroesSaga" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Feedback Hub
+Get-AppxPackage "Microsoft.WindowsFeedbackHub" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.WindowsFeedbackHub" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Get Help
+Get-AppxPackage "Microsoft.GetHelp" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.GetHelp" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Get Office, and it's "Get Office365" notifications
+Get-AppxPackage "Microsoft.MicrosoftOfficeHub" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.MicrosoftOfficeHub" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Get Started
+Get-AppxPackage "Microsoft.GetStarted" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.GetStarted" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Maps
+Get-AppxPackage "Microsoft.WindowsMaps" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.WindowsMaps" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall March of Empires
+Get-AppxPackage "*.MarchofEmpires" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "*.MarchofEmpires" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Messaging
+Get-AppxPackage "Microsoft.Messaging" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.Messaging" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Microsoft To Do
+Get-AppxPackage "Microsoft.Todos" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.Todos" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Mixed Reality Portal
+Get-AppxPackage "Microsoft.MixedReality.Portal" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.MixedReality.Portal" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Mobile Plans
+Get-AppxPackage "Microsoft.OneConnect" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.OneConnect" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall People
+Get-AppxPackage "Microsoft.People" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.People" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Print3D
+Get-AppxPackage "Microsoft.Print3D" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.Print3D" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Skype
+Get-AppxPackage "Microsoft.SkypeApp" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.SkypeApp" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall SlingTV
+Get-AppxPackage "*.SlingTV" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "*.SlingTV" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Solitaire
+Get-AppxPackage "Microsoft.MicrosoftSolitaireCollection" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.MicrosoftSolitaireCollection" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Spotify
+Get-AppxPackage "SpotifyAB.SpotifyMusic" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "SpotifyAB.SpotifyMusic" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall StickyNotes
+Get-AppxPackage "Microsoft.MicrosoftStickyNotes" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.MicrosoftStickyNotes" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Sway
+Get-AppxPackage "Microsoft.Office.Sway" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.Office.Sway" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Twitter
+Get-AppxPackage "*.Twitter" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "*.Twitter" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Voice Recorder
+Get-AppxPackage "Microsoft.WindowsSoundRecorder" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.WindowsSoundRecorder" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Windows Phone Companion
+Get-AppxPackage "Microsoft.WindowsPhone" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.WindowsPhone" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall XBox
+Get-AppxPackage "Microsoft.XboxApp" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.XboxApp" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Your Phone
+Get-AppxPackage "Microsoft.YourPhone" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.YourPhone" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Zune Music (Groove)
+Get-AppxPackage "Microsoft.ZuneMusic" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.ZuneMusic" | Remove-AppxProvisionedPackage -Online
+
+# Uninstall Zune Video
+Get-AppxPackage "Microsoft.ZuneVideo" -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -Online | Where DisplayNam -like "Microsoft.ZuneVideo" | Remove-AppxProvisionedPackage -Online
