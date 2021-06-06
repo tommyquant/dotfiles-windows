eaac779e70048ec150f356864040c89a3b3b8c1e
Tommy <51040159+tommyquant@users.noreply.github.com>
2021-06-06 13:03:59 +1000

    Update packages and .bashrc

--- install.sh
+++ install.sh
@@ -6,21 +6,25 @@ source .env
 set +o allexport
 
 ###############################################################################
+# Dotfiles
+###############################################################################
+
+echo 'Copying dotfiles...'
+cp -R ./.dotfiles/. ~/
+
+###############################################################################
 # Base packages
 ###############################################################################
+
+echo 'Installing base packages...'
 
 sudo apt update
 
 # GCC compiler
 sudo apt install -y build-essential
 sudo apt install -y curl
-sudo apt install -y fonts-powerline
 sudo apt install -y git
-sudo apt install -y locales
-sudo apt install -y zsh
-
-# Install Oh My Zsh. This needs curl, git and zsh packages installed first.
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo apt install -y pulseaudio
 
 ###############################################################################
 # Dev dependencies
@@ -28,18 +32,22 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 echo 'Installing dev dependencies...'
 
-# Install Node
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install -y nodejs
+# NVM and Node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.bashrc
+nvm install node
 
-# Install Yarn
+# Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install -y yarn
 
-# Install Rust
+# Rust
 export RUSTUP_IO_THREADS=1
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
+
+# Sciter
+git clone --depth 1 https://github.com/c-smile/sciter-js-sdk ~/sciter
 
 ###############################################################################
 # Additional setup
@@ -47,11 +55,7 @@ curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
 
 echo 'Performing additional setup...'
 
-# Set up locale needed for Oh My Zsh themes
-locale-gen en_US.UTF-8
 # Configure Git user
+echo 'Configuring Git user...'
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
-
-echo 'Copying dotfiles...'
-cp -R ./.dotfiles/. ~/
